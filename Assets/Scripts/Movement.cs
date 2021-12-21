6a5e37ddd645f1f72812b8c565db51c2dbd1e57c
lidoratias <44170022+lidoratias@users.noreply.github.com>
2021-12-21 15:21:05 +0200

    Initial commit
    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 15.0f;
+    public Rigidbody rb;
+    public Vector2 movement;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Debug.Log(movement.x + "," + movement.y + "," + animator.GetInteger("Speed"));
+        if (movement.x > 0)
+            animator.SetInteger("Speed", 1);
+        else if (movement.x < 0)
+            animator.SetInteger("Speed", -1);
+        else
+            animator.SetInteger("Speed", 0);
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.velocity = this.speed * direction;
+    }
+}
