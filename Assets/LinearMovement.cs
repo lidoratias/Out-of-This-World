55921e15028993219a0323a137512963ec597a69
lidoratias <44170022+lidoratias@users.noreply.github.com>
2022-01-03 12:39:59 +0200

    Added linear movement for the chest
    The new class doesn't use (yet) the whole structure of Shape, Path and their specifications

---
+++ Assets/LinearMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovement : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+    public Rigidbody rb;
+    public Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector2(0.0f, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 pos = Camera.main.WorldToViewportPoint(rb.position);
+        Debug.Log(pos);
+        Debug.Log(movement);
+        
+        if (pos.y < 0.3f)
+        {
+            movement.Set(0.0f, 1.0f);
+        }
+        
+        if (pos.y > 0.8f)
+        {
+            movement.Set(0.0f, -1.0f);
+        }
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.velocity = this.speed * direction;
+    }
+}
