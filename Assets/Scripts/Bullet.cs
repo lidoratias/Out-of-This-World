73c45bb6b9e235de3a4f5b6617f9e897258d5f59
lidoratias <44170022+lidoratias@users.noreply.github.com>
2022-01-06 13:58:19 +0200

    Direction of a bullet is now a parameter
    Direction of a bullet is now a parameter, so we will be able to shoot in which direction that we want.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,11 +7,12 @@ public class Bullet : MonoBehaviour
     public float speed = 20.0f;
     public int damage = 20;
     public Rigidbody2D rb;
+    public Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = transform.right * speed;
+        rb.velocity = direction * speed;
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
