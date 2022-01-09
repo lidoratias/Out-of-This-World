937d92fe181356918dc73add562249d75097facd
lidoratias <44170022+lidoratias@users.noreply.github.com>
2022-01-09 22:58:35 +0200

    Fine tuned some values to make the level easier (for now)
    Fine tuned some values to make the level easier (for now)

--- Assets/Scripts/LinearBullet.cs
+++ Assets/Scripts/LinearBullet.cs
@@ -8,7 +8,7 @@ public class LinearBullet : Bullet
     protected override void Start()
     {
         this.target = "Enemy";
-        this.speed = 20.0f;
+        this.speed = 50.0f;
         this.direction = transform.right;
         rb.velocity = this.direction * this.speed;
     }
