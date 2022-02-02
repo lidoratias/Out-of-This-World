4c499b31d53015856c5b796570ecf4b35b9b150b
lidoratias <44170022+lidoratias@users.noreply.github.com>
2022-02-03 01:37:02 +0200

    Simple Ghost Explosion Added.
    Simple Ghost Explosion Added using a white cloud fading away after collision with the player.
    Now only the swords are left, and then portals and sounds for the level.

--- Assets/Scripts/ExplosionHandler.cs
+++ Assets/Scripts/ExplosionHandler.cs
@@ -7,14 +7,8 @@ public class ExplosionHandler : MonoBehaviour
 
     public Animator anim;
 
-    public float timer = 0;
+    private float timer = 0;
     public float waitingTime = 0.1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public GameObject explosionEffect;
+    public GameObject parentGhost;
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.tag == "Player")
+        {
+            Vector3 explosionPosition = new Vector3(transform.position.x, transform.position.y + 1.0f,
+                transform.position.z);
+            GameObject explosionInstance = Instantiate(explosionEffect, explosionPosition,
+                new Quaternion(0, 0, 0, 1));
+            explosionInstance.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
+            Destroy(parentGhost);
+        }
+    }
+}
