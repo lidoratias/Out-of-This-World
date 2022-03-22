e001633bdfa365d477695eef9e05a53443734938
lidoratias <44170022+lidoratias@users.noreply.github.com>
2022-03-22 19:12:15 +0200

    Hearts spawner now working, needs balancing
    Hearts spawner now working, needs balancing.

--- Assets/Scripts/Level 2/Heart.cs
+++ Assets/Scripts/Level 2/Heart.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 public class Heart : HittingEnemy
 {
     public Animation anim;
-    public AnimationClip heartBeatClip;
+    //public AnimationClip heartBeatClip;
+    public LinearMovement linearMovement;
     private float targetY;
     private int numOfBeats;
+    private bool isBeating = false;
 
+    void Awake()
+    {
+        targetY = Random.Range(-3.5f, 5.5f);
+        numOfBeats = Random.Range(2, 6);
+    }
     // Update is called once per frame
     override protected void Update()
     {
-        if (transform.position.y > targetY)
+        if (transform.position.y > targetY && !isBeating)
         {
-            Invoke("heartBeat", 0.5f);
+            isBeating = true;
+            linearMovement.enabled = false;
+            Invoke("heartBeat", 0.2f);
+        }
+
+        if (numOfBeats == 0 && !anim.isPlaying)
+        {
+            linearMovement.enabled = true;
         }
     }
 
     void heartBeat()
     {
-        anim.Play(anim.clip.ToString());
+        anim.Play();
         numOfBeats--;
         if (numOfBeats > 0)
         {
-            Invoke("heartBeat", anim.clip.length);
+            Invoke("heartBeat", anim.clip.length - 0.5f);
         }
     }
 }
